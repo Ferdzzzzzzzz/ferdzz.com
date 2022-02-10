4c4d8641a0cb9fb75b70fe4b2707702b3c22baa9
Ferdz <ferdz.steenkamp@gmail.com>
2022-02-10 15:53:26 +0200

    massive transfer

---
+++ prod/go/apps/api/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var build = "develop"
+
+func main() {
+	err := run()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	fmt.Println(build)
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	server := http.Server{
+		Addr: ":4000",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hello World")
+		}),
+	}
+
+	fmt.Println("listening at :4000")
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	select {
+	case err := <-serverErrors:
+		return err
+
+	case <-shutdown:
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		err := server.Shutdown(ctx)
+		if err != nil {
+			server.Close()
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ prod/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/ferdzzzzzzzz/ferdzz.com/go
+
+go 1.17
